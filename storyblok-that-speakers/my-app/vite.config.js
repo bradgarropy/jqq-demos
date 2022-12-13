6ae6b7a4178e58339cf23e9e7580943a1933df38
Brad Garropy <bgarropy@atlassian.com>
2022-12-13 15:51:47 -0600

    visual editing.

--- storyblok-that-speakers/my-app/vite.config.js
+++ storyblok-that-speakers/my-app/vite.config.js
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), mkcert()],
+	server: {
+		https: true
+	}
 };
 
 export default config;
