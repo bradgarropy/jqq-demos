ff6b09327b019ab2758e81d338f86e69ba2ce34b
Brad Garropy <bradgarropy@gmail.com>
2023-01-23 21:18:41 -0600

    create routes.

---
+++ altogic-email-verification/my-app/src/routes/failure/+page.svelte
@@ -0,0 +1,3 @@
+<div class="grid h-screen justify-items-center items-center">
+    <span class="font-semibold py-4 px-8 bg-red-600 text-white">Failed to verify your email.</span>
+</div>
---
+++ altogic-email-verification/my-app/src/routes/signup/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div class="grid content-center justify-center h-screen gap-8">
+    <h1 class="text-4xl font-semibold">Sign Up</h1>
+
+    <form>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email">
+        <button type="submit" class="py-2 px-3 bg-blue-500 text-white font-semibold hover:bg-blue-800">Let's go!</button>
+    </form>
+</div>
